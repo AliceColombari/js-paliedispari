9b54b4fd8d7c48e57ea37ad5c954b8145c68e264
AliceColombari <colombari.23@gmail.com>
2022-01-31 10:21:52 +0100

    alternativa -correzione mattinata

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -52,3 +52,27 @@ function invertiParola(str){
   var strInversa = str.split('').reverse().join('');  
   return strInversa;
 }
+
+
+// alternativa per verifica
+/*
+function controlloPalindroma(parolaUtente) {
+
+  let parolaDaConfrontare = '';
+
+  for(let i = parolaUtente. length -1; i>=0; i--) {
+  parolaDaConfrontare += parolaUtente[i];
+  }
+
+  if (parolaUtente.toUpperCase() == parolaDaConfrontare.toUpperCase()) {
+  return "La parola inseria è palindroma";
+  } else {
+  return "La parola inseria non è palindroma";
+}
+
+}
+
+let parolaUtente = prompt ('Inserisci una parola') ;
+let verifica = controlloPalindroma (parolaUtente) ;
+console. log(verifica);
+*/
